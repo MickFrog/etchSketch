7dc5a2f041652b9894961e425500b64856136d14
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-01-09 19:55:41 +0300

    Acquire drawBoard div and dimensions in JS

---
+++ script.js
@@ -0,0 +1,6 @@
+//Acquire container div and dimensions
+const container = document.getElementById("drawBoard");
+if (container) {
+    containerWidth = container.clientWidth;
+    containerHeight = container.clientHeight;
+}
