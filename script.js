a66be9d2538789959886ec1daf92a762418fe6d5
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-01-09 22:41:10 +0300

    Add change in color when mouse over drawBoard

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const container = document.getElementById("drawBoard");
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
-    
+
+let intervID;  //Global Changing div color interval ID
+
 fillBoard(64);
 
 function fillBoard(size) {
@@ -15,12 +17,18 @@ function fillBoard(size) {
 
 //Draw a single element div
 function drawBox(size){ 
-    let childDiv = document.createElement('div');
+    let childDiv = document.createElement('div');  //Single div to be manipulated for drawBoard
     if(childDiv) {
         // childDiv.style.background = "black";
         childDiv.style.width = `${containerWidth/size}px`;
         childDiv.style.height = `${containerHeight/size}px`;
+
+        childDiv.addEventListener('mouseover', function(event) {
+            if(event.buttons == 1) {
+                childDiv.style.background = "black";
+            }
+        });
+
         container.appendChild(childDiv);
     }
-
 }
