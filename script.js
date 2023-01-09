a7585a3a37480d4de31c8201fb397b9544aa95da
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-01-09 20:03:07 +0300

    attach div to drawboard - fix drawboard display

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //Acquire container div and dimensions
 const container = document.getElementById("drawBoard");
 if (container) {
-    containerWidth = container.clientWidth;
-    containerHeight = container.clientHeight;
+    const containerWidth = container.clientWidth;
+    const containerHeight = container.clientHeight;
+    
+    //Create a single div
+    let drawDiv = document.createElement('div');
+    if(drawDiv) {
+        drawDiv.clientHeight = containerHeight / 16;
+        drawDiv.clientWidth = containerWidth / 16;
+    }
+    drawDiv.style.background = "black";
+    container.appendChild(drawDiv);
 }
